d669b059e9c4be00222712306b36e4ee998b71e2
Faisal Ahmed <faaisal8898@gmail.com>
2025-08-02 01:35:12 +0600

    added cors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import http from "http";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 const PORT = process.env.PORT || 5000;
